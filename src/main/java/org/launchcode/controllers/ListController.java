6b1d7d221c7a60453558bfbea945c29ec4562d3c
Hugo <connorbird.hugo@gmail.com>
2018-06-14 18:50:47 -0500

    First functioning copy

--- src/main/java/org/launchcode/controllers/ListController.java
+++ src/main/java/org/launchcode/controllers/ListController.java
@@ -40,13 +40,17 @@ public class ListController {
         if (column.equals("all")) {
             ArrayList<HashMap<String, String>> jobs = JobData.findAll();
             model.addAttribute("title", "All Jobs");
+            model.addAttribute("jobsLength", jobs.size());
             model.addAttribute("jobs", jobs);
+
+
             return "list-jobs";
         } else {
             ArrayList<String> items = JobData.findAll(column);
             model.addAttribute("title", "All " + columnChoices.get(column) + " Values");
             model.addAttribute("column", column);
             model.addAttribute("items", items);
+
             return "list-column";
         }
 
@@ -58,7 +62,9 @@ public class ListController {
 
         ArrayList<HashMap<String, String>> jobs = JobData.findByColumnAndValue(column, value);
         model.addAttribute("title", "Jobs with " + columnChoices.get(column) + ": " + value);
+        model.addAttribute("jobsLength", jobs.size());
         model.addAttribute("jobs", jobs);
+
 
         return "list-jobs";
     }
--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -4,10 +4,13 @@ import org.launchcode.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+
+import static org.launchcode.models.JobData.findByColumnAndValue;
 
 /**
  * Created by LaunchCode
@@ -16,12 +19,19 @@ import java.util.HashMap;
 @RequestMapping("search")
 public class SearchController {
 
+
     @RequestMapping(value = "")
     public String search(Model model) {
         model.addAttribute("columns", ListController.columnChoices);
         return "search";
     }
+    @RequestMapping(value = "results", method = RequestMethod.GET)
+    public String search(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+        ArrayList<HashMap<String, String>> jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+        model.addAttribute("jobs", jobs);
+        model.addAttribute("columns", ListController.columnChoices);
+        return "search";
+            // TODO #1 - Create handler to process search request and display results
+        }
+    }
 
-    // TODO #1 - Create handler to process search request and display results
-
-}
